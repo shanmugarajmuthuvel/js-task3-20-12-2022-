35b92c4b0e9584a48a87303f7d3acd288bb6f385
shanmugarajmuthuvel <shanmugarajmuthuvel1777@yahoo.com>
2022-12-20 16:42:28 +0530

    find odd or even with array

---
+++ test3.js
@@ -0,0 +1,21 @@
+var a=[1,2,3,4,5,6,7,8,9,10]
+var b=[]
+var c=[]
+var d=0
+var e=0
+
+  for(i=0; i<a.length; i++){
+	  if(a[i]%2==0){
+		  b[d]=a[i]
+		  d++
+	  }
+	  else{
+          c[e]=a[i]
+          e++		  
+
+  }
+  }
+  document.write(b)
+  document.write("<br>")
+  document.write(c)
+	  
